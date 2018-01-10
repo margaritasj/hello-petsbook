db8ff44b65840108cb020f78c72b3b051d9d077a
margb13 <marg.b13@hotmail.com>
2018-01-10 01:22:09 -0500

    Mejorando vista profile

--- js/login.js
+++ js/login.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+  /* ------------ deshabilitando el boton iniciar sesion ------------ */
+  $('#btn-login').prop('disabled', false);
 
+  
+  $('#btn-login').click(function() {
+    window.location.href = '../views/profile.html';
+  }); 
 });
