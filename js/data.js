ff5f69a811785bacb39ea4a6c39087ac1b0c2bb9
NishiAicchi <ai.espichan@gmail.com>
2018-01-10 13:54:57 -0500

    agregando data

---
+++ js/data.js
@@ -0,0 +1,63 @@
+data = {
+  'contacts': {
+    'followers': [
+      {
+        'username': 'Bernardino',
+        'dog breed': 'Beagle',
+        'age': '2 años',
+        'adopted': 'no'
+      },
+
+      {
+        'username': 'princesa',
+        'dog breed': 'Caniche',
+        'age': '1 año',
+        'adopted': 'yes'
+      },
+
+      {
+        'username': 'Cataleya',
+        'dog breed': 'Chihuahua',
+        'age': '4 año',
+        'adopted': 'yes'
+      },
+
+      {
+        'username': 'Chavo',
+        'dog breed': 'Grand Danes',
+        'age': '6 año',
+        'adopted': 'no'
+      }
+    ],
+
+    'following': [
+      {
+        'username': 'Peludito',
+        'dog breed': 'Chow Chow',
+        'age': '2 año',
+        'adopted': 'yes'
+      }, 
+
+      {
+        'username': 'Pongo',
+        'dog breed': 'Border Collie',
+        'age': '4 año',
+        'adopted': 'no'
+      },
+
+      {
+        'username': 'Diana',
+        'dog breed': 'Golden Retriever',
+        'age': '1 año',
+        'adopted': 'no'
+      }, 
+
+      {
+        'username': 'Caifas',
+        'dog breed': 'Akita',
+        'age': '3 año',
+        'adopted': 'yes'
+      }
+    ]
+  }
+};
